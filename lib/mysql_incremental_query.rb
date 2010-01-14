bb977c0c85e869cee54b97ee588e96a519d4cf73
Burke Libbey <burke@burkelibbey.org>
2010-01-14 15:34:37 -0600

    reverting query_with_result should be in the ensure block too

--- lib/mysql_incremental_query.rb
+++ lib/mysql_incremental_query.rb
@@ -2,13 +2,13 @@ class Mysql
   def incremental_query(query, &block)
     result = nil;
     def result.free;end
+    qwr = self.query_with_result
     begin
-      qwr = self.query_with_result
       self.query_with_result = false
       result = self.query(query).use_result
       result.each { |row| yield(row) }
-      self.query_with_result = qwr
     ensure
+      self.query_with_result = qwr
       result.free
     end
   end
